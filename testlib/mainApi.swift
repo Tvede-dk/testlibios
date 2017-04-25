f43d59c05a3027e750d5c16fb74560027a9fd090
Kasper Tvede <kasper@webmasteren.eu>
2017-04-25 15:15:58 +0200

    start / test.

---
+++ testlib/mainApi.swift
@@ -0,0 +1,21 @@
+//
+//  mainApi.swift
+//  testlib
+//
+//  Created by Kasper T on 25/04/2017.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import Foundation
+
+public class mainApi {
+    
+    
+    public var varaible : String = ""
+    
+    
+    public func work() -> String{
+        return "hello from main api :)"
+    }
+    
+}
---
+++ testlib/testlib.h
@@ -0,0 +1,19 @@
+//
+//  testlib.h
+//  testlib
+//
+//  Created by Kasper T on 25/04/2017.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testlib.
+FOUNDATION_EXPORT double testlibVersionNumber;
+
+//! Project version string for testlib.
+FOUNDATION_EXPORT const unsigned char testlibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testlib/PublicHeader.h>
+
+
